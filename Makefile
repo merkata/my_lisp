e30f9f51c18b64ca8eeca4972a6d398a07acfaa2
Mariyan Dimitrov <mddimitrov@telenor.bg>
2017-04-21 11:30:11 +0300

    MPC compiles - chapter 6

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 hello_world: hello_world.c
 	cc -I/usr/local/include -std=c99 -Wall -pedantic -Wextra hello_world.c -o hello_world
 
-parsing: parsing.c
-	cc -L/usr/local/lib -I/usr/local/include -std=c99 -Wall -pedantic -Wextra -lm -lreadline parsing.c -o parsing
+parsing: parsing.c mpc.c
+	cc -L/usr/local/lib -I/usr/local/include -std=c99 -Wall -pedantic -Wextra -lm -lreadline mpc.c parsing.c -o parsing
--- parsing.c
+++ parsing.c
@@ -4,7 +4,28 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#include "mpc.h"
+
 int main(int argc, char **argv) {
+  //
+//MPC parser init - parsers
+
+mpc_parser_t *Number = mpc_new("number");
+mpc_parser_t *Expr = mpc_new("expr");
+mpc_parser_t *Operator = mpc_new("operator");
+mpc_parser_t *Lispy = mpc_new("lispy");
+
+//define language of parsers
+
+mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispy);
+
 
   puts("Lispy version 0.0.1");
   puts("Press Ctrl-C to quit\n");
@@ -19,6 +40,8 @@ int main(int argc, char **argv) {
     free(input); //frees memory
 
   }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
   return 0;
 
