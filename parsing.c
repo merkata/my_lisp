7986b14ef9d3e3e7ce9e08f548388861ef7675d3
Mariyan Dimitrov <mddimitrov@telenor.bg>
2017-04-21 16:40:12 +0300

    Evaluation of AST - chapter 7

--- parsing.c
+++ parsing.c
@@ -6,6 +6,9 @@
 
 #include "mpc.h"
 
+long eval(mpc_ast_t *t);
+long eval_op(long x, char *op, long y);
+
 int main(int argc, char **argv) {
   //
 //MPC parser init - parsers
@@ -35,7 +38,19 @@ mpca_lang(MPCA_LANG_DEFAULT,
 
     add_history(input);
 
-    printf("No you're a %s\n", input);
+    //parse user input
+    mpc_result_t r;
+    if(mpc_parse("<stdin>", input, Lispy, &r)) {
+      //parsed successfully
+      //mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
+      mpc_ast_delete(r.output);
+    } else {
+      //we did not parse correctly
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
 
     free(input); //frees memory
 
@@ -45,4 +60,28 @@ mpca_lang(MPCA_LANG_DEFAULT,
 
   return 0;
 
+}
+
+long eval(mpc_ast_t *t) {
+  if(strstr(t->tag, "number")) { return atoi(t->contents); }
+
+  char *op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+long eval_op(long x, char *op, long y) {
+  if(strcmp(op, "+") == 0 ) { return (x + y); }
+  if(strcmp(op, "-") == 0 ) { return (x - y); }
+  if(strcmp(op, "*") == 0 ) { return (x * y); }
+  if(strcmp(op, "/") == 0 ) { return (x / y); }
+  return 0;
 }
